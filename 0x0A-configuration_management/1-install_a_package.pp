f249cae2e8bb3c453dec0920ac0353e7dadd65f9
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-30 08:49:05 +0200

    Fix package dependency and update Flask version

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,12 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Install a specific version of flask (2.1.0)
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  before => Werkzeug
+  before => 'Werkzeug'
 }
+
 package {'Werkzeug':
   ensure => '2.2.2',
   provider => 'pip3',
