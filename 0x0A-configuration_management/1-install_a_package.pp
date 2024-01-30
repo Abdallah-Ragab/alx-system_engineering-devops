6b32da26723ad699cda98c3a7dae7d2b7fa6ec3a
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-30 08:44:43 +0200

    Add package installation for Werkzeug 2.2.2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,10 @@
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  before => Werkzeug
+}
+package {'Werkzeug':
+  ensure => '2.2.2',
+  provider => 'pip3',
 }
