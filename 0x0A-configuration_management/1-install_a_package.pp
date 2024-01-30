3a429b98314ad7b65116a8dc00ec80ac1320ab76
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-30 09:03:23 +0200

    indent

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # Install a specific version of flask (2.1.0)
 
 package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  before => 'Werkzeug'
+  ensure    => '2.1.0',
+  provider  => 'pip3',
+  before    => 'Werkzeug'
 }
 
 package {'Werkzeug':
-  ensure => '2.2.2',
-  provider => 'pip3',
+  ensure    => '2.2.2',
+  provider  => 'pip3'
 }
