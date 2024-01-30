741f3b29eeceb51498f96e274495ec1c38c8ce6d
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-30 09:19:30 +0200

    fuck this shit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,18 @@
-# Install a specific version of flask (2.1.0)
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  before   => 'Werkzeug'
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package {'Werkzeug':
-  ensure   => '2.2.2',
-  provider => 'pip3'
+# Install Flask 2.1.0
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.2.2
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.2.2',
+  creates => '/usr/local/lib/python3.8/dist-packages/Werkzeug-2.2.2.dist-info',
+  require => Package['python3-pip'],
 }
